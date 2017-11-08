e8cded6579a4df3afb02485ac9e8bd2eacc8b9e6
Wilson Birney <wpb@360scada.com>
2017-11-07 21:50:35 -0500

    added collision checking, so we will never overwrite a paste

--- src/paste_id.rs
+++ src/paste_id.rs
@@ -1,5 +1,7 @@
 use rand::{self,Rng};
 
+use std::path::Path;
+
 /// Table to retrieve base62 values from.
 const BASE62: &'static [u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -12,13 +14,22 @@ pub fn generate_id(size: usize) -> String {
     id
 }
 
+fn check_for_id(id: &str) -> bool {
+	Path::new(&format!("upload/{}", id)).exists()
+}
+
 pub struct PasteId {
 	id: String
 }
 
 impl PasteId {
 	pub fn generate() -> PasteId {
-		PasteId{ id: generate_id(3) }
+		let mut id = generate_id(3);
+		while check_for_id(&id)	{
+			println!("collision detected");
+			id = generate_id(3);
+		}
+		PasteId{ id: id }
 	}
 	pub fn id(&self) -> String { self.id.clone() }
 	pub fn filename(&self) -> String { format!("upload/{}", self.id) }
