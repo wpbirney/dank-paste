54c2aefef4ed2b63027b8841ed311d10d357a615
Wilson Birney <wpb@360scada.com>
2017-11-13 22:52:58 -0500

    paste_dog now handles both paste and urls

--- src/paste_dog.rs
+++ src/paste_dog.rs
@@ -8,8 +8,8 @@ use std::path::Path;
 use std::time::Duration;
 use std::thread::{self, JoinHandle};
 
-use info::PasteInfo;
-use id::{DankId,PasteId};
+use info::{PasteInfo,UrlInfo};
+use id::{DankId,PasteId,UrlId};
 
 //maximum allowed age in seconds
 pub const MAX_AGE: u64 = 259200;
@@ -31,42 +31,50 @@ fn get_age(path: &Path) -> Option<u64> {
 	Some(modified.elapsed().ok()?.as_secs())
 }
 
-//delete the paste at fp if expired
-fn delete_if_expired(fp: &Path, paste: &PasteId)	{
-	let age = get_age(&fp).unwrap();
-	let info = PasteInfo::load(fp.to_str().unwrap());
+macro_rules! delete_if_expired {
+	($fp:expr, $id:expr, $info:tt) => {
+		let age = get_age($fp).unwrap();
+		let info = $info::load($fp.to_str().unwrap());
 
-	if info.expire == 0 {
-		if age > MAX_AGE {
-			paste.delete_all();
+		if info.expire == 0 {
+			if age > MAX_AGE {
+				$id.delete_all();
+			}
+		} else {
+			if age > info.expire {
+				$id.delete_all();
+			}
 		}
-	} else {
-		if age > info.expire {
-			paste.delete_all();
-		}
-	}
+	};
 }
 
-fn walk() {
+fn walk_paste() {
 	for path in fs::read_dir("upload").unwrap() {
-		let path = path.unwrap();
-
-		let fp = path.path();
+		let fp = path.unwrap().path();
 
 		if let Some(ext) = fp.extension() {
 			let paste = PasteId::from_id(fp.file_stem().unwrap().to_str().unwrap()).unwrap();
 			if ext == "del" {
 				paste.delete_all();
 			} else if ext == "json" {
-				delete_if_expired(&fp, &paste);
+				delete_if_expired!(&fp, paste, PasteInfo);
 			}
 		}
+	}
+}
+
+fn walk_url() {
+	for path in fs::read_dir("shorts").unwrap() {
+		let fp = path.unwrap().path();
+		let url = UrlId::from_id(fp.file_name().unwrap().to_str().unwrap()).unwrap();
+		delete_if_expired!(&fp, url, UrlInfo);
 	}
 }
 
 fn paste_dog()  {
 	loop {
 		thread::sleep(Duration::from_secs(INTERVAL));
-		walk();
+		walk_paste();
+		walk_url();
 	}
 }
