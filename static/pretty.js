2e5b0426e120fafde1ef9863753a95c23c48d6ee
Wilson Birney <wpb@360scada.com>
2017-12-03 21:38:44 -0500

    removed tabs

--- static/pretty.js
+++ static/pretty.js
@@ -1,5 +1,5 @@
 /*
-	Handles keepn' shit pretty
+    Handles keepn' shit pretty
 */
 var pick = document.getElementById("lang-pick");
 var link = document.getElementById('link');
@@ -9,41 +9,41 @@ var orig = link.href;
 var ID = orig.split('/').reverse()[0];
 var plugins = ['lang-rust', 'lang-css'];
 
-function removeAllPlugins()	{
-	for ( var i = 0; i < plugins.length; ++i )	{
-		codebox.classList.remove(plugins[i]);
-	}
+function removeAllPlugins()    {
+    for ( var i = 0; i < plugins.length; ++i )    {
+        codebox.classList.remove(plugins[i]);
+    }
 }
 
-function updateLang(reload)	{
-	switch(pick.value)	{
-		case "rust":
-			codebox.classList.add('lang-rust');
-			link.href = orig + '?lang=rust';
-			break;
-		case "css":
-			codebox.classList.add('lang-css');
-			link.href = orig + '?lang=css';
-			break;
-		default:
-			removeAllPlugins();
-			link.href = orig;
-			break;
-	}
+function updateLang(reload)    {
+    switch(pick.value)    {
+        case "rust":
+            codebox.classList.add('lang-rust');
+            link.href = orig + '?lang=rust';
+            break;
+        case "css":
+            codebox.classList.add('lang-css');
+            link.href = orig + '?lang=css';
+            break;
+        default:
+            removeAllPlugins();
+            link.href = orig;
+            break;
+    }
 
-	if(reload) {
-		document.location = link.href;
-	}
+    if(reload) {
+        document.location = link.href;
+    }
 }
 
 var params = new URLSearchParams(window.location.search);
 
 var q = params.get('lang');
-if(q != null)	{
-	pick.value = q;
+if(q != null)    {
+    pick.value = q;
 }
 updateLang(false);
 
 document.getElementById("lang-pick").addEventListener('change', function() {
-	updateLang(true);
+    updateLang(true);
 });
